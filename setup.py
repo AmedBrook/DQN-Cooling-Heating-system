5a5181c5c765fa5a21df0351907df7402c0b79fe
Ahmed <mabrook.amed@gmail.com>
2023-03-31 17:37:46 +0300

    Project start

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='DQN-based cooling & heating system ',
+    author='Ahmed Mabrouk',
+    license='BSD-3',
+)
