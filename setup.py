e97d25efecd098013d30f6dc6534479a61347757
Ahmed <mabrook.amed@gmail.com>
2023-04-03 00:22:42 +0300

    Updating setup file

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
+import setuptools
 from setuptools import find_packages, setup
 
+
 setup(
-    name='src',
-    packages=find_packages(),
-    version='0.1.0',
+    name='qdnc',
     description='DQN-based cooling & heating system ',
     author='Ahmed Mabrouk',
     license='BSD-3',
+    packages=setuptools.find_packages(include=['src', 'src.models','src.features', 'src.data'] ),
 )
