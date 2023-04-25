04cc91c22f4402a244c51d4707570328db64d20b
Ahmed <mabrook.amed@gmail.com>
2023-04-25 23:23:56 +0300

    Updating the packages source directory

--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
 import setuptools
-from setuptools import find_packages, setup
+from setuptools import find_namespace_packages, setup
+
+def get_install_requirements():    
+
+    with open("requirements.txt", "r", encoding="utf-8") as f:        
+        reqs = [x.strip() for x in f.read().splitlines()]    
+        reqs = [x for x in reqs if not x.startswith("#")]    
+    return reqs
 
 
 setup(
@@ -7,5 +14,6 @@ setup(
     description='DQN-based cooling & heating system ',
     author='Ahmed Mabrouk',
     license='BSD-3',
-    packages=setuptools.find_packages(include=['src', 'src.models','src.features', 'src.data'] ),
+    packages=setuptools.find_namespace_packages(include=['src_dqnc', 'src_dqnc.models','src_dqnc.features', 'src_dqnc.data'] ),
+    install_requires= get_install_requirements()
 )
